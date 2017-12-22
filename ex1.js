0115c8fc2cc5ef91f3cee058b6dd512ddda9a89a
Eric Glover <etgt28@mail.missouri.edu>
2017-12-21 18:12:42 -0600

    heroku things

--- ex1.js
+++ ex1.js
@@ -6,17 +6,27 @@ const https = require("https");
 app.get("/", (req, res) => {
   res.end("hello no encryption yet");
 });
-
+const production = process.env.PORT ? true : false;
 const host = "127.0.01";
-const port = "3000";
-const port1 = "8080";
+const port = production ? process.env.PORT : "3000";
+const port1 = process.env.PORT || "8080";
 
 // app.listen(port, host, () => console.log(`listening on ${host}:${port}`));
-http
-  .createServer(app)
-  .listen(port, host, () => console.log(`listening on ${host}:${port}`));
-https
-  .createServer(app)
-  .listen(port1, host, () =>
-    console.log(`secure, listening on ${host}:${port1}`)
-  );
+
+if (production) {
+  http
+    .createServer(app)
+    .listen(port, () => console.log(`listening on ${host}:${port}`));
+  https
+    .createServer(app)
+    .listen(port, () => console.log(`secure, listening on ${host}:${port1}`));
+} else {
+  http
+    .createServer(app)
+    .listen(port, host, () => console.log(`listening on ${host}:${port}`));
+  https
+    .createServer(app)
+    .listen(port1, host, () =>
+      console.log(`secure, listening on ${host}:${port1}`)
+    );
+}
