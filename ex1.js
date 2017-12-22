fcc79633770891cb2276644c0b99088105ee9f0a
Eric Glover <etgt28@mail.missouri.edu>
2017-12-21 18:40:15 -0600

    self-signed functioning

--- ex1.js
+++ ex1.js
@@ -3,23 +3,34 @@ const app = express();
 const http = require("http");
 const https = require("https");
 
-app.get("/", (req, res) => {
+app.get("/*", (req, res) => {
+  console.log("req occurred ");
   res.end("hello no encryption yet");
 });
 const production = process.env.PORT ? true : false;
+console.log(`port = ${process.env.PORT}`);
 const host = "127.0.01";
 const port = production ? process.env.PORT : "3000";
 const port1 = process.env.PORT || "8080";
 
+//get cert and key
+const fs = require("fs");
+
+const options = {
+  key: fs.readFileSync("./key.pem"),
+  cert: fs.readFileSync("./certificate.pem")
+};
+//
+// console.log(fs.readFileSync("./certificate.pem"));
 // app.listen(port, host, () => console.log(`listening on ${host}:${port}`));
 
 if (production) {
-  http
-    .createServer(app)
-    .listen(port, () => console.log(`listening on ${host}:${port}`));
+  // http
+  //   .createServer(app)
+  //   .listen(port, () => console.log(`listening on ${host}:${port}`));
   https
-    .createServer(app)
-    .listen(port, () => console.log(`secure, listening on ${host}:${port1}`));
+    .createServer(options, app)
+    .listen(port, () => console.log(`secure, listening on ${port1}`));
 } else {
   http
     .createServer(app)
--- selfSign.js
+++ selfSign.js
@@ -1,5 +1,13 @@
 /*
-notes on self-signing certs 
-resources : 
+notes on self-signing certs
+resources :
 https://www.digitalocean.com/community/tutorials/openssl-essentials-working-with-ssl-certificates-private-keys-and-csrs
+https://devcenter.heroku.com/articles/ssl-certificate-self
+stormy-shelf-81243.herokuapp.com/
+
+
+
+//generate csr 
+openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem
+
 */
